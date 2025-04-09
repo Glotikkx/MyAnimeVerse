2f6f1c98cb31b17e1e3cbe238393112b1ed9391c
Glotikkx <chenyuwang21@gmail.com>
2025-04-09 13:00:40 -0400

    ok i lied it works now

--- next.config.ts
+++ next.config.ts
@@ -7,23 +7,13 @@ const nextConfig = {
         hostname: 's4.anilist.co',
         pathname: '/**',
       },
-    ],
-  },
-};
-
-module.exports = nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConf = {
-  images: {
-    remotePatterns: [
       {
-        protocol: "https",
-        hostname: "uploads.mangadex.org",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'uploads.mangadex.org',
+        pathname: '/**',
       },
     ],
   },
 };
 
-module.exports = nextConf;
+module.exports = nextConfig;
